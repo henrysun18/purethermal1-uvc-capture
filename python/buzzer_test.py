40d017b9cbe3b7b106940d036e1ded5e8ebd28a9
henrysun18 <sh4sun@edu.uwaterloo.ca>
2019-07-25 21:35:26 -0400

    fix buzzer; add enable and disable flag as well as stop() function for future coordination with PIR sensor

--- python/buzzer_test.py
+++ python/buzzer_test.py
@@ -1,7 +1,3 @@
-from gpiozero import TonalBuzzer
-from gpiozero.tones import Tone
-from gpiozero.tools import sin_values
-
 import RPi.GPIO as GPIO
 from time import sleep
 import time
@@ -9,36 +5,35 @@ import time
 class Buzzer:
     def __init__(self, pin):
         self.buzzer = pin
-
-        GPIO.setmode(GPIO.BOARD)
-        GPIO.setup(buzzer, GPIO.OUT)
+        self.enable = True
 
         self.delay = 0.8 # delay in seconds
         self.time_between_speedups = 3; # in seconds
         self.min_delay = 0.049 # delay in seconds
 
-        self.time_since_speed_increase = time.time() #facilitate increasing speed of beeps
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setup(self.buzzer, GPIO.OUT)
 
-    def play(seconds):
+    def play(self, seconds):
+        self.enable = True
+
         start_time = time.time()
-        try:
-            while True:
-                current_time = time.time()
-                print("current_time - time_since_speed_increase = ", current_time, " - ", time_since_speed_increase, " = ", int(current_time - time_since_speed_increase))
-                if int(current_time - time_since_speed_increase) >= time_between_speedups:
-                    delay = max(min_delay, delay/2)
-                    time_since_speed_increase = current_time
+        time_since_speed_increase = time.time() #facilitate increasing speed of beeps
+        while True:
+            current_time = time.time()
+            print("current_time - time_since_speed_increase = ", current_time, " - ", time_since_speed_increase, " = ", int(current_time - time_since_speed_increase))
+            if int(current_time - time_since_speed_increase) >= self.time_between_speedups:
+                self.delay = max(self.min_delay, self.delay/2)
+                time_since_speed_increase = current_time
+            GPIO.output(self.buzzer, GPIO.HIGH)
+            sleep(self.delay)
+            GPIO.output(self.buzzer, GPIO.LOW)
+            sleep(self.delay)
 
-                GPIO.output(buzzer, GPIO.HIGH)
-                sleep(delay)
-                GPIO.output(buzzer, GPIO.LOW)
-                sleep(delay)
-
-                if int(current_time - start_time > seconds):
-                    break # stop playing after we played enough seconds as requested
-        finally:
-            GPIO.cleanup()
-
+            if not self.enable or int(current_time - start_time > seconds):
+                break 
+    def stop(self):
+        self.enable = False
 
 if __name__ == '__main__':
     Buzzer(7).play(30)
